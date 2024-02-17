46207a927cd2ea333b85a694aeaa0c9ec137a844
mpbarlow <mpbarlow@users.noreply.github.com>
2024-02-17 22:17:00 +0000

    deploy: 5ce93b0bc263899abcd391ef9f305f29dc49d3d2

--- src/tags/tags.11tydata.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  pagination: {
-    data: "collections.posts",
-    // Extract all the unique tags out of all the pages
-    before: (pages) => [...new Set(pages.map((page) => page.data.post_tags).flat())],
-    size: 1,
-    alias: "tag",
-  },
-};
