7456a26130d67fee484a3fbe34e3dd77e5f87382
mpbarlow <matt@barlow.coffee>
2024-02-17 22:13:50 +0000

    support github pages

--- .eleventy.js
+++ .eleventy.js
@@ -4,25 +4,27 @@ const rss = require("@11ty/eleventy-plugin-rss");
 const dayjs = require("dayjs");
 dayjs.extend(require("dayjs/plugin/advancedFormat"));
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(rss);
+module.exports = function (config) {
+  config.addPlugin(syntaxHighlight);
+  config.addPlugin(rss);
 
-  eleventyConfig.addPassthroughCopy("src/font");
-  eleventyConfig.addPassthroughCopy("src/img");
-  eleventyConfig.addPassthroughCopy("src/css/prism-one-light.css");
-  eleventyConfig.addPassthroughCopy("src/css/prism-material-oceanic.css");
+  // Needed to GitHub Pages hosting
+  config.addPassthroughCopy("src/CNAME");
 
-  eleventyConfig.addGlobalData("currentYear", new Date().getFullYear());
-  eleventyConfig.addGlobalData("siteUrl", "https://barlow.dev");
+  config.addPassthroughCopy("src/robots.txt");
+  config.addPassthroughCopy("src/font");
+  config.addPassthroughCopy("src/img");
+  config.addPassthroughCopy("src/css/prism-one-light.css");
+  config.addPassthroughCopy("src/css/prism-material-oceanic.css");
+
+  config.addGlobalData("currentYear", new Date().getFullYear());
+  config.addGlobalData("siteUrl", "https://barlow.dev");
 
   // Filter to posts within the last year
-  eleventyConfig.addFilter("recent", (posts) =>
-    posts.filter((post) => dayjs().diff(dayjs(post.data.date), "year") < 1),
-  );
+  config.addFilter("recent", (posts) => posts.filter((post) => dayjs().diff(dayjs(post.data.date), "year") < 1));
 
   // Given an array of posts, group by their publish date in the form e.g. "January 2024"
-  eleventyConfig.addFilter("groupByYearMonth", (posts) =>
+  config.addFilter("groupByYearMonth", (posts) =>
     posts.reduce((carry, current) => {
       const group = dayjs(current.data.date).format("MMMM YYYY");
 
@@ -37,22 +39,22 @@ module.exports = function (eleventyConfig) {
   );
 
   // Filter a collection of posts by those that include the specified tag
-  eleventyConfig.addFilter("filterByTag", (posts, tag) => posts.filter((post) => post.data.post_tags.includes(tag)));
+  config.addFilter("filterByTag", (posts, tag) => posts.filter((post) => post.data.post_tags.includes(tag)));
 
   // Given a set of posts, extract all unique tags
-  eleventyConfig.addFilter("extractTags", (posts) => [...new Set(posts.map((post) => post.data.post_tags).flat())]);
+  config.addFilter("extractTags", (posts) => [...new Set(posts.map((post) => post.data.post_tags).flat())]);
 
   // Format a Y-m-d date into the format displayed on posts, e.g. "1st Jan 2024"
-  eleventyConfig.addFilter("postDate", (ymd) => dayjs(ymd).format("Do MMM YYYY"));
+  config.addFilter("postDate", (ymd) => dayjs(ymd).format("Do MMM YYYY"));
 
   // Strip out code from page content for the purposes of estimating reading time
-  eleventyConfig.addFilter("textOnly", (content) => content.replace(/<pre class=(.|\n)*?<\/pre>/gm, ""));
+  config.addFilter("textOnly", (content) => content.replace(/<pre class=(.|\n)*?<\/pre>/gm, ""));
 
   // Compute an estimated reading time assuming 225 words/min
-  eleventyConfig.addFilter("readingTime", (wordCount) => `${Math.max(1, Math.floor(wordCount / 225))} minute`);
+  config.addFilter("readingTime", (wordCount) => `${Math.max(1, Math.floor(wordCount / 225))} minute`);
 
   // Convert a given number to Roman numerals
-  eleventyConfig.addFilter("asRomanNumerals", (number) => {
+  config.addFilter("asRomanNumerals", (number) => {
     const numerals = {
       M: 1000,
       CM: 900,
